58cdedd7ddca1d438ed3f56506b9bd3bad2ec91f
StevenYuan666 <ye.yuan3@mail.mcgill.ca>
2023-04-02 11:16:39 -0400

    Complete the all basic functionalities

--- src/main/java/socs/network/node/Router.java
+++ src/main/java/socs/network/node/Router.java
@@ -72,6 +72,52 @@ public class Router {
     System.out.println(path);
   }
 
+  private boolean requestLinkDeletion(String processIP, short processPort, String simulatedIP){
+    try{
+      // Setup the socket used for communication and corresponding input and output
+      Socket clientSocket = new Socket(processIP, processPort);
+      ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+      ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream());
+
+      // Construct a deletion packet
+      SOSPFPacket deletion = new SOSPFPacket();
+      deletion.srcProcessIP = this.rd.processIPAddress;
+      deletion.srcProcessPort = this.rd.processPortNumber;
+      deletion.srcIP = this.rd.simulatedIPAddress;
+      deletion.dstIP = simulatedIP;
+      deletion.sospfType = 3; // That is a deletion PACKET
+      deletion.routerID = deletion.srcIP; // According to the explanation of TA on myCourses
+      deletion.neighborID = deletion.dstIP;
+
+      // Send the deletion request to the remote router
+      outputStream.writeObject(deletion);
+
+      try{
+        SOSPFPacket receivedPacket = (SOSPFPacket) inputStream.readObject();
+        if (receivedPacket.sospfType == 3) { //i.e., received a deletion reply
+          // Close all the socket and streams
+          clientSocket.close();
+          inputStream.close();
+          outputStream.close();
+          return true;
+        }
+        else{
+          // Close all the socket and streams
+          clientSocket.close();
+          inputStream.close();
+          outputStream.close();
+          return false;
+        }
+      }
+      catch (Exception e){
+        throw new RuntimeException(e);
+      }
+    }
+    catch (Exception e){
+      throw new RuntimeException(e);
+    }
+  }
+
   /**
    * disconnect with the router identified by the given destination ip address
    * Notice: this command should trigger the synchronization of database
@@ -79,6 +125,30 @@ public class Router {
    * @param portNumber the port number which the link attaches at
    */
   private void processDisconnect(short portNumber) {
+    //check to make sure the port number is valid
+    // that it is not null
+    // and that there actually exists a two-way link
+    if (portNumber > 3 || portNumber < 0 || ports[portNumber] == null
+            || ports[portNumber].router2.status != RouterStatus.TWO_WAY) {
+      System.err.println("Invalid port error.");
+      return;
+    }
+
+    RouterDescription rd = ports[portNumber].router2;
+
+    //get in touch with this router so you can send link deletion request
+    if (requestLinkDeletion(rd.processIPAddress, rd.processPortNumber, rd.simulatedIPAddress)) {
+      this.ports[portNumber] = null;
+
+      try {
+        // Do the synchronization
+        synchronize();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    } else {
+      System.err.println("Error while trying to delete link.");
+    }
 
   }
 
@@ -340,6 +410,16 @@ public class Router {
     }
   }
 
+  public int getPort(String dest) {
+    for (int i = 0; i < 4; i++) {
+      if (this.ports[i] != null && this.ports[i].router2.simulatedIPAddress.equals(dest)) {
+        return i;
+      }
+    }
+    System.err.println("Specified IP does not exist.");
+    return -1;
+  }
+
   /**
    * attach the link to the remote router, which is identified by the given simulated ip;
    * to establish the connection via socket, you need to indentify the process IP and process Port;
@@ -349,7 +429,198 @@ public class Router {
    */
   private void processConnect(String processIP, short processPort,
                               String simulatedIP, short weight) {
+    if (this.started) {
 
+      //check to make sure you aren't connecting to yourself
+      if (simulatedIP.equals(this.rd.simulatedIPAddress)) {
+        System.err.println("You can't connect to yourself!");
+        return;
+      }
+
+      //check to make sure isn't already connected to requested remote router
+      for (int x = 0; x < 4; x++) {
+        if (this.ports[x] != null && this.ports[x].router2.simulatedIPAddress.equals(simulatedIP)) {
+          System.err.println("You are already connected to this router!");
+          return;
+        }
+      }
+
+      // setup RouterDescription for the desired router
+      RouterDescription remote = new RouterDescription();
+      remote.processIPAddress = processIP;
+      remote.processPortNumber = processPort;
+      remote.simulatedIPAddress = simulatedIP;
+
+      // find first available port
+      // Find the first available port
+      int available_port = -1;
+      for(int i = 0; i < 4; i ++){
+        if (this.ports[i] == null){
+          available_port = i;
+          break;
+        }
+      }
+
+      // print out error if there's no more ports available
+      if (available_port == -1){
+        System.err.println("THERE IS NO PORT AVAILABLE RIGHT NOW!");
+        return;
+      }
+
+      // attempt to connect with desired router
+      try {
+        Socket clientSocket = new Socket(processIP, processPort);
+        ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+        ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream());
+
+        // make sure the router is "connectable"
+        // Forward some information for the router to be attached
+        outputStream.writeObject(this.rd.simulatedIPAddress);
+        // Also forward the port number since the server will add corresponding link to its ports array
+        outputStream.writeObject(this.rd.processPortNumber);
+        // Also forward the weight of the link
+        outputStream.writeObject(weight);
+
+        try {
+          String incoming = (String) inputStream.readObject();
+
+          if (incoming.equals("1")) {
+            System.out.println("Your connect request has been accepted;");
+            // if all goes well, assign the new router link to the available port
+            this.ports[available_port] = new Link(rd, remote, weight);
+            // Close all the socket and streams
+            clientSocket.close();
+            inputStream.close();
+            outputStream.close();
+          } else {
+            System.err.println("Your connect request has been rejected;");
+            System.err.println(incoming);
+          }
+        }
+        catch (Exception e){
+          throw new RuntimeException(e);
+        }
+
+      }
+      catch (Exception e){
+        throw new RuntimeException(e);
+      }
+
+      //Basically do start except with the messaging back and forth
+      Socket clientSocket = null;
+      ObjectOutputStream outputStream = null;
+      ObjectInputStream inputStream = null;
+
+
+      // Initialization section:
+      // Try to open a socket on the given port
+      // Try to open input and output streams
+      String hostname = this.ports[available_port].router2.processIPAddress;
+      short port = this.ports[available_port].router2.processPortNumber;
+
+      try {
+        clientSocket = new Socket(hostname, port);
+        outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+        inputStream = new ObjectInputStream(clientSocket.getInputStream());
+      }
+      catch (Exception e){
+        throw new RuntimeException(e);
+      }
+
+      // If everything has been initialized then we want to send a packet
+      // to the socket we have opened a connection to on the given port
+      try {
+        SOSPFPacket packet = new SOSPFPacket();
+        packet.srcProcessIP = this.rd.processIPAddress;
+        packet.srcProcessPort = this.rd.processPortNumber;
+        packet.srcIP = this.rd.simulatedIPAddress;
+        packet.dstIP = this.ports[available_port].router2.simulatedIPAddress;
+        packet.sospfType = 2; // That is a CONNECT packet
+        packet.routerID = packet.srcIP; // According to the explanation of TA on myCourses
+        packet.neighborID = packet.dstIP;
+        packet.HelloWeight = weight;
+
+        //broadcast the HELLO packet
+        try {
+          outputStream.writeObject(packet);
+        } catch (NullPointerException e) {
+          // Close all the socket and streams
+          clientSocket.close();
+          inputStream.close();
+          outputStream.close();
+          System.err.println("Trying to send a null packet!");
+          return;
+        }
+
+        //wait for response
+        Object incoming_unk;
+
+        try {
+          incoming_unk = inputStream.readObject();
+        }
+        catch (Exception e){
+          // Close all the socket and streams
+          clientSocket.close();
+          inputStream.close();
+          outputStream.close();
+          throw new RuntimeException(e);
+        }
+
+        SOSPFPacket incoming;
+
+        // this should only happen if receiving "Ports are full error". Use case ends
+        if (incoming_unk instanceof String) {
+          String temp = (String) incoming_unk;
+
+          //need to delete current link from ports since it is impossible to have future conversation with this router
+          this.ports[available_port] = null;
+
+          System.out.println(temp + " Deleting link reference from port. Maybe try to connect again later.");
+          // Close all the socket and streams
+          clientSocket.close();
+          inputStream.close();
+          outputStream.close();
+          return;
+
+          //otherwise the HELLO packet was processed successfully
+        } else {
+          incoming = (SOSPFPacket) incoming_unk;
+        }
+
+        //check to make sure the packet received was a HELLO
+        if (incoming == null || incoming.sospfType != 2) {
+          System.out.println("Error: did not receive a CONNECT back!");
+
+          // clean up
+          // Close all the socket and streams
+          clientSocket.close();
+          inputStream.close();
+          outputStream.close();
+          return;
+        }
+
+        this.ports[available_port].router1.status = RouterStatus.TWO_WAY;
+        this.ports[available_port].router2.status = RouterStatus.TWO_WAY;
+
+        //send back the HELLO packet
+        outputStream.writeObject(packet);
+
+        //broadcast LSAUPDATE to neighbors
+        synchronize();
+
+        // Close all the socket and streams
+        clientSocket.close();
+        inputStream.close();
+        outputStream.close();
+
+      }
+      catch (Exception e){
+        throw new RuntimeException(e);
+      }
+
+    } else {
+      System.err.println("You must run start command before attempting to connect!");
+    }
   }
 
   /**
@@ -383,11 +654,38 @@ public class Router {
 
   }
 
+  private boolean isConnected(){
+    for (Link port : this.ports) {
+      if (port != null && port.router1.status == RouterStatus.TWO_WAY) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   /**
    * disconnect with all neighbors and quit the program
    */
   private void processQuit() {
+    //check to make sure you are connected to any remote routers
+    if (!isConnected()) {
+      System.out.println("Quitting process was successful.");
+      System.exit(0);
+    }
 
+    //if you are connected to remote routers, then you need to disconnect from each one before exiting
+    for (int i = 0; i < 4; i++) {
+
+      //you can skip any null or non-started ports
+      if (ports[i] == null || ports[i].router2.status != RouterStatus.TWO_WAY) continue;
+
+      processDisconnect((short) i);
+
+    }
+
+    System.out.println("Quitting process was successful.");
+
+    System.exit(0);
   }
 
   /**
@@ -396,6 +694,19 @@ public class Router {
   private void updateWeight(String processIP, short processPort,
                              String simulatedIP, short weight){
 
+  }
+
+  private void processPortInfo(){
+    int portNum = 0;
+    for (Link l : this.ports){
+      if (l == null) {
+        System.out.println("The Router Connected in Port " + portNum + " is: None");
+      }
+      else{
+        System.out.println("The Router Connected in Port " + portNum + " is: " + l.router2.simulatedIPAddress);
+      }
+      portNum ++;
+    }
   }
 
   public void terminal() {
@@ -424,7 +735,7 @@ public class Router {
         } else if (command.equals("start")) {
           processStart();
           System.out.print(">> ");
-        } else if (command.equals("connect ")) {
+        } else if (command.startsWith("connect ")) {
           String[] cmdLine = command.split(" ");
           processConnect(cmdLine[1], Short.parseShort(cmdLine[2]),
                   cmdLine[3], Short.parseShort(cmdLine[4]));
@@ -437,6 +748,10 @@ public class Router {
           signal = 1;
         } else if (command.equals("N")) {
           signal = 0;
+        } else if (command.equals("portInfo")) {
+          //output port information
+          processPortInfo();
+          System.out.print(">> ");
         } else {
           //invalid command
           break;
